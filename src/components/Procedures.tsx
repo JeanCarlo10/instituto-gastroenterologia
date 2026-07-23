ca4655ca3a5bb792be9209631e8003c11ceacc87
Jean Carlo Balduino <jean.balduino@truechange.com>
2026-07-23 18:10:07 -0300

    Ajuste de animação

--- src/components/Procedures.tsx
+++ src/components/Procedures.tsx
@@ -111,16 +111,17 @@ const Procedures = () => {
           whileInView="animate"
           viewport={{
             once: true,
+            amount: 0.5,
           }}
           className="mt-20 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
         >
-          {procedures.map((procedure, index) => {
+          {procedures.map((procedure) => {
             const Icon = procedure.icon;
 
             return (
               <motion.a
                 key={procedure.title}
-                variants={RevealCard(index * 0.08)}
+                variants={RevealCard()}
                 {...CardHover}
                 target="_blank"
                 rel="noopener noreferrer"
